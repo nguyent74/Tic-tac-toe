7840235f5ff42cac6468452152d36f60fb62db03
nguyent74 <nguyent74@BY1928A75002023.wit.private>
2020-12-16 18:31:41 -0500

    tictactoe pro project

--- src/tictactoe/tictactoepro.java
+++ src/tictactoe/tictactoepro.java
@@ -20,64 +20,63 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+public class tictactoepro extends Application {
 
-public class tictactoepro extends Application { 
-	
 	private boolean playable = true;
 	private boolean turnX = true;
-	private Tile[][] board = new Tile [3][3];
+	private Tile[][] board = new Tile[3][3];
 	private List<Combo> combos = new ArrayList<>();
-	
+
 	Pane root = new Pane();
-	
-	
+
 	private Parent createContent() {
-		
+
 		root.setPrefSize(600, 600);
-		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
 				Tile tile = new Tile();
-				tile.setTranslateX(j *200);
-				tile.setTranslateY(i *200);
-				
+				tile.setTranslateX(j * 200);
+				tile.setTranslateY(i * 200);
+
 				root.getChildren().add(tile);
-				
-				board [j][i] = tile;
+
+				board[j][i] = tile;
 			}
 		}
-		
-		for(int y = 0; y < 3; y++) {
+
+		for (int y = 0; y < 3; y++) {
 			combos.add(new Combo(board[0][y], board[1][y], board[2][y]));
 		}
-		
-		for(int x = 0; x < 3; x++) {
-			combos.add(new Combo(board[x] [0], board[x][1], board[x][2]));
+
+		for (int x = 0; x < 3; x++) {
+			combos.add(new Combo(board[x][0], board[x][1], board[x][2]));
 		}
-		
-		
+
 		combos.add(new Combo(board[0][0], board[1][1], board[2][2]));
 		combos.add(new Combo(board[2][0], board[1][1], board[0][2]));
-		
+
 		return root;
-		
+
 	}
-	@Override 
+
+	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setScene(new Scene(createContent()));
 		primaryStage.show();
-	
+
 	}
+
 	private void checkStage() {
 		for (Combo combo : combos) {
-			if(combo.isComplete()) {
+			if (combo.isComplete()) {
 				playable = false;
 				playWinanimation(combo);
 				break;
 			}
 		}
 	}
-	
+
 	private void playWinanimation(Combo combo) {
 		Line line = new Line();
 		line.setStartX(combo.tiles[0].getCenterX());
@@ -85,59 +84,57 @@ public class tictactoepro extends Application {
 		line.setEndX(combo.tiles[0].getCenterX());
 		line.setEndY(combo.tiles[0].getCenterY());
 		line.setStroke(Color.RED);
-		
+
 		root.getChildren().add(line);
-		
+
 		Timeline timeline = new Timeline();
-		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2),
-				new KeyValue(line.endXProperty(), combo.tiles[2].getCenterX()),
-				new KeyValue(line.endYProperty(), combo.tiles[2].getCenterY())));
+		timeline.getKeyFrames()
+				.add(new KeyFrame(Duration.seconds(2), new KeyValue(line.endXProperty(), combo.tiles[2].getCenterX()),
+						new KeyValue(line.endYProperty(), combo.tiles[2].getCenterY())));
 		timeline.play();
 	}
-	
+
 	private class Combo {
 		private Tile[] tiles;
+
 		public Combo(Tile... tiles) {
 			this.tiles = tiles;
 		}
-		
+
 		public boolean isComplete() {
 			if (tiles[0].getValue().isEmpty())
 				return false;
-			
-			return tiles[0].getValue().equals(tiles[1].getValue())
-					&& tiles[0].getValue().equals(tiles[2].getValue());
+
+			return tiles[0].getValue().equals(tiles[1].getValue()) && tiles[0].getValue().equals(tiles[2].getValue());
 		}
-		
+
 	}
-	
+
 	private class Tile extends StackPane {
 		private Text text = new Text();
-			
-				
+
 		public Tile() {
 			Rectangle border = new Rectangle(200, 200);
 			border.setFill(null);
 			border.setStroke(Color.BLACK);
-			
+
 			text.setFont(Font.font(60));
-			
+
 			setAlignment(Pos.CENTER);
 			getChildren().addAll(border, text);
-			
-			setOnMouseClicked(event ->{
-				if(!playable)
+
+			setOnMouseClicked(event -> {
+				if (!playable)
 					return;
-				
-				if (event.getButton()== MouseButton.PRIMARY) {
-					if(!turnX)
+
+				if (event.getButton() == MouseButton.PRIMARY) {
+					if (!turnX)
 						return;
 					drawX();
 					turnX = false;
 					checkStage();
-				}
-				else if (event.getButton()== MouseButton.SECONDARY) {
-					if(turnX)
+				} else if (event.getButton() == MouseButton.SECONDARY) {
+					if (turnX)
 						return;
 					drawO();
 					turnX = true;
@@ -145,37 +142,35 @@ public class tictactoepro extends Application {
 				}
 			});
 		}
-		
+
 		public double getCenterX() {
-			return getTranslateX() + 100; 
-			}
-			
+			return getTranslateX() + 100;
+		}
+
 		public double getCenterY() {
 			return getTranslateY() + 100;
-					
-			
+
 		}
-		
+
 		public String getValue() {
 			return text.getText();
 		}
-		
+
 		private void drawX() {
 			text.setText("x");
 			text.setStroke(Color.RED);
 		}
+
 		private void drawO() {
 			text.setText("O");
 			text.setStroke(Color.BLUE);
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		launch(args);
-		
 
 	}
 
-	
 }
